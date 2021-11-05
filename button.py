378ee20d295b5c54b3a1f94895b11108b5e8ce0c
Wikignometry <pphanach@andrew.cmu.edu>
2021-11-05 14:13:10 -0400

    added draw method + test functions

--- button.py
+++ button.py
@@ -1,10 +1,15 @@
+###################################################################
+#       Imported Files
+from cmu_112_graphics import *
+from draw_helpers import *
+###################################################################
 
 # Button is the superclass for every pressable object in the program
 #           –> button must be rectangular
 
 class Button():
 
-    def __init__(self, location, dimensions, action):
+    def __init__(self, location, dimensions, action, fill='blue', outline='black'):
         # tuples(x, y) of center of button or None 
         self.location = location
 
@@ -13,6 +18,10 @@ class Button():
 
         # functions that perform a given action when the button is pressed
         self.action = action
+
+        # color name
+        self.fill = fill
+        self.outline = outline
 
     # returns True if the button isPressed
     #       –> assumes that button is rectangular (doesn't account for rounded corners)
@@ -23,7 +32,17 @@ class Button():
         return (abs(self.location[0] - x) < self.width/2 and
                 abs(self.location[1] - y) < self.height/2)
 
-#TODO: setLocation function??
+    # draws a rounded square button
+    def draw(self, canvas):
+        if self.location == None: return # does not draw if location is None
+        x, y = self.location
+        create_roundedRectangles(canvas, 
+                                x - self.width/2, y - self.height/2,
+                                x + self.width/2, y + self.height/2,
+                                r=10, fill=self.fill, outline=self.outline)
+
+
+
 
 ###################################################################
 #       Test Functions
@@ -39,9 +58,20 @@ def testButtonClass():
     assert(button1.isPressed(5, 27) == False)
     print('Passed!')
 
+# def appStarted(app):
+#     app.button = Button((100,100), (50,50), lambda: print('foo'), fill='royalBlue')
+
+# def mousePressed(app, event):
+#     if app.button.isPressed(event.x, event.y):
+#         app.button.action()
+
+# def redrawAll(app, canvas):
+#     app.button.draw(canvas)
+
 ###################################################################
 #       Code to run
 
 testButtonClass()
+# runApp(width=200, height=200)
 
 
