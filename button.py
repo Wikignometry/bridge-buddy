ad1d891fc500a90cb696e0676306f5c9ffad69d1
Wikignometry <pphanach@andrew.cmu.edu>
2021-11-04 23:55:11 -0400

    made button support None location
    changed the self.x and self.y to self.location; added condition to check for None in isPressed

--- button.py
+++ button.py
@@ -5,20 +5,23 @@
 class Button():
 
     def __init__(self, location, dimensions, action):
-        # locations are input as tuples(x, y) of center of button
-        self.x, self.y = location
+        # tuples(x, y) of center of button or None 
+        self.location = location
 
-        # dimensions are input as tuples(width, height)
+        # tuples(width, height)
         self.width, self.height = dimensions
 
-        # actions are functions that perform a given action when the button is pressed
+        # functions that perform a given action when the button is pressed
         self.action = action
 
     # returns True if the button isPressed
     #       –> assumes that button is rectangular (doesn't account for rounded corners)
     def isPressed(self, x, y):
-        return (abs(self.x - x) < self.width/2 and
-                abs(self.y - y) < self.height/2)
+        # to prevent indexOutOfBounds error
+        if self.location == None: 
+            return False
+        return (abs(self.location[0] - x) < self.width/2 and
+                abs(self.location[1] - y) < self.height/2)
 
 
 ###################################################################
@@ -28,8 +31,8 @@ def testButtonClass():
     print('Testing Button...', end='')
     button1 = Button((10,20), (5,10), lambda: 'foo')
     assert(button1.action() == 'foo')
-    assert(button1.x == 10)
-    assert(button1.y == 20)
+    assert(button1.location[0] == 10)
+    assert(button1.location[1] == 20)
     assert(button1.isPressed(12, 21) == True)
     assert(button1.isPressed(9, 18) == True)
     print('Passed!')
