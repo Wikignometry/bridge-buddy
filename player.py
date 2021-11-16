3d957bd016e811619086c8f70c2eaecc713d3f5f
Wikignometry <pphanach@andrew.cmu.edu>
2021-11-16 15:44:22 -0500

    Create player.py

---
+++ player.py
@@ -0,0 +1,14 @@
+# Player is the class for all human players, and the superclass for the AI
+
+
+
+class Player():
+
+    #TODO: more parameters to come
+    def __init__(self, username):
+        self.username = username
+
+    def __eq__(self, other):
+        return isinstance(other, Player) and self.username == other.username
+
+    
