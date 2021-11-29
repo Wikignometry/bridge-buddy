6a6f334f482dda7059c06ac6b2666d109bac95fc
Wikignometry <pphanach@andrew.cmu.edu>
2021-11-29 16:01:27 -0500

    better heuristics (I think)

--- heuristic.py
+++ heuristic.py
@@ -1,12 +1,26 @@
 ###################################################################
 #       Imported Files
-from node import *
+from card import *
 ###################################################################
 
 # super simple heuristic right now
 def baseHeuristic(node):
     # positive if ns gains more tricks than ew
-    return node.nsTricks - node.ewTricks
+    heuristic = node.nsTricks - node.ewTricks
+    number = 14
+    highestTrump = Card(number, node.bid.trump)
+    if highestTrump in node.hands['n'] and highestTrump in node.hands['s']:
+        while highestTrump in node.hands['n'] and highestTrump in node.hands['s']:
+            heuristic += 1
+            number -= 1
+            highestTrump = Card(number, node.bid.trump)
+    else:
+        while highestTrump in node.hands['e'] and highestTrump in node.hands['w']:
+            heuristic -= 1
+            number -= 1
+            highestTrump = Card(number, node.bid.trump)
+    return heuristic
+
 
 # make this better
 
